3bc91161fdafc41471246f603dbda0427dc16fb1
Guilherme Branco <guilhermejbranco@Guilhermes-MBP.lan>
2020-01-05 16:23:57 +0000

    Quick patch for issue #1, has to be fixed later

--- src/App.js
+++ src/App.js
@@ -25,27 +25,29 @@ const hash = window.location.hash
     return initial;
   }, {});
 
-window.location.hash = "";
+
 
 class App extends Component {
   constructor() {
-  super();
-  this.state = {
-    token: null,
-  item: {
-    album: {
-      images: [{ url: "" }]
+    super();
+    this.state = {
+      token: null,
+    item: {
+      album: {
+        images: [{ url: "" }]
+      },
+      name: "",
+      artists: [{ name: "" }],
+      duration_ms:0,
     },
-    name: "",
-    artists: [{ name: "" }],
-    duration_ms:0,
-  },
-  is_playing: "Paused",
-  progress_ms: 0
-};
-this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-}
-getCurrentlyPlaying(token) {
+    is_playing: "Paused",
+    progress_ms: 0
+    };
+
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }
+
+  getCurrentlyPlaying(token) {
     // Make a call using the token
     $.ajax({
       url: "https://api.spotify.com/v1/me/player",
@@ -73,25 +75,25 @@ getCurrentlyPlaying(token) {
       });
     }
   }
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      {!this.state.token && (
-        <a
-          className="btn btn-primary"
-          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-        >
-          Login to Spotify
-        </a>
-      )}
-      {this.state.token && (
-        <div>LOGGED SUCCESSFULLY</div>
-      )}
-      </header>
-    </div>
-  );
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        {!this.state.token && (
+          <a
+            className="btn btn-primary"
+            href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+        )}
+        {this.state.token && (
+          <div>LOGGED SUCCESSFULLY</div>
+        )}
+        </header>
+      </div>
+    );
+    }
   }
-}
 export default App;
