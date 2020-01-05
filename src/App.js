710c03c6731120cd9c6031809687e9f8fe0a5c0c
Guilherme Branco <guilhermejbranco@Guilhermes-MBP.lan>
2020-01-05 15:49:56 +0000

    added bootstrap support

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="btn btn-primary">Login with Spotify</button>
       </header>
     </div>
   );
